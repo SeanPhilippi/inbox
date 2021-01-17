14f98f67acf39045d86d076f4a75c12a00af2cc7
SeanPhilippi <sean.philippi@gmail.com>
2021-01-17 15:45:23 -0600

    fixes, and first real test, calling method of Inbox contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 beforeEach(async () => {
@@ -12,11 +14,20 @@ beforeEach(async () => {
     // what is passed into the arguments array serves as arguments for the Inbox constructor function
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     // check for existence of what is passed in
     assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    // .call() takes an object where you tell it how that function off the methods
+    // object gets called, like amount of gas and who will pay, but message() isn't
+    // modifying data, so no need
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, 'Hi there!');
   });
 });
