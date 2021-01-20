899e2016f3f611bd98bbe72a216591ce4d1cdb56
SeanPhilippi <sean.philippi@gmail.com>
2021-01-19 20:35:56 -0600

    contract deployment working

--- deploy.js
+++ deploy.js
@@ -1,12 +1,26 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const mneumonic = process.env.MNEUMONIC;
+console.log('mneumonic', mneumonic)
 
-const provider = newHDWalletProvider(
+const provider = new HDWalletProvider(
   mneumonic,
   'https://rinkeby.infura.io/v3/df229ca01f3c474996806f60f74c0e03'
 );
 // instance of web3 enabled for the rinkeby network
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('attempting to deploy from account', accounts[0])
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['yo boi'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address)
+}
+
+deploy();
