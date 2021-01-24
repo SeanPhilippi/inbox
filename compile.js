9b430c0200127dbeeacc5e672c93897c735774dd
SeanPhilippi <sean.philippi@gmail.com>
2021-01-24 15:22:02 -0600

    adjustments to deploy and compile.js for new contract

--- compile.js
+++ compile.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 // solidity compiler
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 // 2nd argument is type of encoding
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
--- deploy.js
+++ deploy.js
@@ -20,7 +20,7 @@ const deploy = async () => {
   console.log('attempting to deploy from account', accounts[0])
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: '0x' + bytecode, arguments: ['yo boi'] })
+    .deploy({ data: '0x' + bytecode })
     .send({ from: accounts[0] });
 
     console.log('Contract deployed to', result.options.address)
