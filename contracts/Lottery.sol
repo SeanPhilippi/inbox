1cabde42d8c30bb3098c917b3fed3b75b9e1289d
SeanPhilippi <sean.philippi@gmail.com>
2021-01-24 15:17:27 -0600

    lottery contract code from remix

---
+++ contracts/Lottery.sol
@@ -0,0 +1,45 @@
+// specify solidity version being used
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    // public variables defined up here have an auto function created that returns that value
+    address public manager;
+    // dynamic array of addresses
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        // used for validation, mass in a value that gets converted to true or false
+        // if true, rest of function executes, if false, exits
+        // require() is used to see that a requirement has been satisfied before executing the rest of a function
+        // default is measured in wei, have to put 'ether' if you want the unit to be ether
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        // keccak256 is essentially the same as sha3 hashing algorithm
+        return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager);
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        // create new players dynamic array with initial length of 0
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+
+    }
+}
--- deploy.js
+++ deploy.js
@@ -1,12 +1,15 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+const dotenv = require('dotenv');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, '.env')});
 
 const mneumonic = process.env.MNEUMONIC;
 console.log('mneumonic', mneumonic)
 
 const provider = new HDWalletProvider(
-  mneumonic,
+  'cloth plunge increase glide round hope achieve fat worth mistake submit equal',
   'https://rinkeby.infura.io/v3/df229ca01f3c474996806f60f74c0e03'
 );
 // instance of web3 enabled for the rinkeby network
@@ -17,8 +20,8 @@ const deploy = async () => {
   console.log('attempting to deploy from account', accounts[0])
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['yo boi'] })
-    .send({ gas: '1000000', from: accounts[0] });
+    .deploy({ data: '0x' + bytecode, arguments: ['yo boi'] })
+    .send({ from: accounts[0] });
 
     console.log('Contract deployed to', result.options.address)
 }
